98ecbeef2e8263f0f74db1b313e605cf5affe9fc
wise <@>
2016-12-10 20:42:16 +0500

    пути подключения xml конфигов

--- lab1/main.py
+++ lab1/main.py
@@ -6,13 +6,14 @@ from PIL import Image
 import subprocess
 index=[]
 images=[]
-text=[]
-cascade = cv.Load('C:\\haarcascade_frontalface_alt.xml')
-cascad = cv.Load('C:\\haarcascade_mcs_eyepair_big.xml')
+
+#cascade=cv.Load(os.path.join(OpenCVRoot, "haarcascades", "haarcascade_frontalface_alt.xml"))
+cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+cascad = cv2.CascadeClassifier('haarcascade_mcs_eyepair_big.xml')
 
 #cascade = cv.Load('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml')
 #cascad = cv.Load('/usr/local/share/OpenCV/haarcascades/haarcascade_mcs_eyepair_big.xml')
-
+cv2.
 def detectImage(image):
     bitmap = cv.fromarray(image)
     faces = cv.HaarDetectObjects(bitmap, cascade, cv.CreateMemStorage(0))
