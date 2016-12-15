1662413250141e74178465c95592829ab3d9d441
wise <@>
2016-12-15 23:17:54 +0500

    починка определения схем по контрастности, добавлен пропущеный пример

--- lab1/main.py
+++ lab1/main.py
@@ -6,6 +6,20 @@ text = []
 face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('./cascades/haarcascade_eye.xml')
 
+
+def detectFileType (image):
+    dist = cv2.calcHist([image],[0],None,[256],[0,256])
+    prev = ''
+    for i in dist:
+        if prev == '':
+            prev = i.real[0]
+        else:
+            if abs(prev-i.real[0]) > 10000.0:
+                return 'schema'
+            prev = i.real[0]
+    return "photo"
+
+
 def detectImage(image):
     faces = face_cascade.detectMultiScale(image, 1.3, 5)
     if type(faces).__name__ == 'ndarray':
@@ -36,19 +50,18 @@ for image_path in image_paths:
     resstr += image_path
     images.append(image_path)
     img = cv2.imread(image_path)
-
     try:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     except cv2.error as e:
         print(image_path + ' is not an image')
         continue
 
-
-    if detectImage(gray):
-        resstr += ' has face'
-
-    if detectEye(gray):
-        resstr += ' has eyes'
+    if (detectImage(gray) == True):
+        resstr = resstr + ' have face '
+    if (detectEye(gray) == True):
+        resstr = resstr + ' have eyes '
+    if (detectFileType(gray) == "schema"):
+        resstr = resstr + ' it is schema '
 
     index.append(resstr)
     resstr = ""
